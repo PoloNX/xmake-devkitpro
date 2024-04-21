76e7e9aaefbd8c593053004e1c026cfd6f829f11
PoloNX <geyrecharlie@gmail.com>
2024-04-21 14:17:16 +0200

    added sdl2 demo

--- xmake.lua
+++ xmake.lua
@@ -1,15 +1,16 @@
 includes("toolchain/*.lua")
-includes("package/*.lua")
+
+add_repositories("switch-repo https://github.com/PoloNX/switch-repo.git")
 
 
-add_requires("switch-curl")
+add_requires("libsdl", "libsdl_mixer", "libsdl_ttf", "libsdl_image")
 
-target("example")
+target("sdl2-demo")
     set_arch("aarch64")
     add_rules("switch")
     set_toolchains("devkita64")
 
-    set_values("switch.name", "example")
+    set_values("switch.name", "sdl2-demo")
     set_values("switch.author", "PoloNX")
     set_values("switch.version", "1.0.0")
     
@@ -18,7 +19,7 @@ target("example")
 
     add_files("example/*.cpp")
 
-    add_packages("switch-curl")
+    add_packages("libsdl", "libsdl_mixer", "libsdl_ttf", "libsdl_image")
 
 
 
